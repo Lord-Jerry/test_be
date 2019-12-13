88f7dbbd7ffaed008191091b583c99a043c46b43
Lord Jerry <lovinjerry004@gmail.com>
2019-12-13 02:52:54 +0100

    implement routes and server file

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { create } = require('../middleware/validation/users');
+const UserController = require('../controllers/users');
+const url = '/api/v1';
+
+// USER ROUTE
+router
+  .route(`${url}/create-user`)
+  .post(
+    create,
+    UserController.create,
+  );
+
+router
+  .route(`${url}/update-user/:userId`)
+  .post(
+    create,
+    UserController.update,
+  );
+
+router
+  .route(`${url}/delete-user/:userId`)
+  .delete(
+    UserController.delete,
+  );
+
+module.exports = router;
