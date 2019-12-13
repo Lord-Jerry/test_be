a712d4c410716cc5d3e7ad802392aadc8c9283e5
Lord Jerry <lovinjerry004@gmail.com>
2019-12-13 03:13:58 +0100

    implement task validation

---
+++ middleware/validation/tasks.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+const Validator = require('validatorjs');
+// const { users } = require('../../models');
+
+const createTask = async (req, _res, next) => {
+  const rules = {
+    task: 'required',
+    description: 'required',
+    state: 'required',
+  };
+  const validate = new Validator(req.body, rules);
+
+  if (!validate.passes()) {
+    const err = new Error();
+    err.message = validate.errors;
+    err.statusCode = 400;
+    return next(err);
+  }
+
+  const state = ['todo', 'done'];
+
+  if (!state.includes(req.body['state'])) {
+    const err = new Error();
+    err.message = 'invalid state';
+    err.statusCode = 400;
+    return next(err);
+  }
+
+  return next();
+};
+
+module.exports = {
+  createTask,
+};
